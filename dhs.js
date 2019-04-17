ed4d63058a62c0b51bf79a451b1b2f1952ea2be7
yuuyuyuko <47532170+yuuyuyuko@users.noreply.github.com>
2019-04-17 15:45:43 +0900

    Update dhs.js

--- dhs.js
+++ dhs.js
@@ -184,23 +184,6 @@ if (
     document.body.appendChild(tool_div);
   } //func-->
   create_toolbox();
-  
-    setTimeout(function() {
-    onkeyup = function() {
-      if (event.keyCode === 49 || event.keyCode === 97) {
-        document.getElementById('cid').value = 47;
-        document.getElementById('c_round').value = 1;
-        document.getElementById('c_pw').value = 'hnfy';
-        init_all();
-      }
-      if (event.keyCode === 50 || event.keyCode === 98) {
-        document.getElementById('cid').value = 48;
-        document.getElementById('c_round').value = 1;
-        document.getElementById('c_pw').value = 'fhxy';
-        init_all();
-      }
-    };
-  }, .5 * 1000);
 } //else-->
 
 function loop() {
