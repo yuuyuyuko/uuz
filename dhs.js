9886af5b1ce4bbd829e830f6e90b4ba16cc832ed
yuuyuyuko <47532170+yuuyuyuko@users.noreply.github.com>
2019-05-04 01:11:44 +0900

    Update dhs.js

--- dhs.js
+++ dhs.js
@@ -26,7 +26,7 @@ setTimeout(function() {
     onkeyup = function() {
         if (event.keyCode === 72) {F(47, 1, 'hnfy')}
         if (event.keyCode === 70) {F(51, 1, 'fhxy')}
-        if (event.keyCode === 68) {F(46, 2, 'dfbm2')}
+        if (event.keyCode === 68) {F(46, 1, 'dfbm2')}
         if (event.keyCode === 81) {F(45, 3, 'qlsqls')}
         if (event.keyCode === 89) {F(38, 6, 'yysyys')}
         if (event.keyCode === 90) {F(50, 1, 'tsdm')}
